d71b1bb5f12d9db2d2c034a842546bea99288c38
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-11-18 22:43:29 +0200

    remove space

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,6 @@
 # This Puppet script sets up the SSH client configuration file.
 # It configures the client to use the private key ~/.ssh/school
 # and refuses to authenticate using a password.
-
 file { '/home/ubuntu/.ssh/config':
   ensure  => present,
   content => "Host *\n  IdentityFile /home/ubuntu/.ssh/school\n  PasswordAuthentication no\n",
