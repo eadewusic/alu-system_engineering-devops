5db5844bc68595e3cf5b60b4cb6282299e784a65
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-11-19 14:37:22 +0200

    Added comments

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -3,18 +3,21 @@
 # and refuses to authenticate using a password.
 
 # Seting up my client config file
+# Include the stdlib module, which provides additional Puppet functions
 include stdlib
 
+# Ensure that the line 'PasswordAuthentication no' exists in the /etc/ssh/ssh_config file
 file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  replace => true,
+  ensure  => present,          # Ensure the line is present in the file
+  path    => '/etc/ssh/ssh_config',  # Specify the path to the configuration file
+  line    => '    PasswordAuthentication no',  # The line to be added or modified
+  replace => true,             # Replace the line if it already exists
 }
 
-file_line { 'Delare identity file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
-  replace => true,
+# Ensure that the line 'IdentityFile ~/.ssh/school' exists in the /etc/ssh/ssh_config file
+file_line { 'Declare identity file':
+  ensure  => present,          # Ensure the line is present in the file
+  path    => '/etc/ssh/ssh_config',  # Specify the path to the configuration file
+  line    => '     IdentityFile ~/.ssh/school',  # The line to be added or modified
+  replace => true,             # Replace the line if it already exists
 }
