e80eb4ef97eddaf8edfe43a7029239b0491f3d82
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-11-19 02:31:14 +0200

    Update 100-puppet_ssh_config.pp

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -2,6 +2,10 @@
 # It configures the client to use the private key ~/.ssh/school
 # and refuses to authenticate using a password.
 
-Host *
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no",
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+}
