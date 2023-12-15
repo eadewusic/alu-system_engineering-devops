b47da4bc4675335a32ba47e1c8e3ff714dad11ae
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-12-16 01:55:18 +0200

    Update 0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,7 @@
 # 0-strace_is_your_friend.pp
-# Puppet manifest to fix PHP fatal error in Apache
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-exec { 'fix_php_error':
-  command => 'sed -i s/phpp/php/g /var/www/html/index.php',
-  path    => '/usr/local/bin/:/bin/',
-  notify  => Service['apache2'],
-}
-
-service { 'apache2':
-  ensure => running,
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
