0ed5aeed7610dfab40313da5f26336224eb34b94
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-12-16 01:47:28 +0200

    Update 0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,12 @@
 # 0-strace_is_your_friend.pp
 # Puppet manifest to fix PHP fatal error in Apache
 
-# Define an exec resource to replace the deprecated mysql_connect function
 exec { 'fix_php_error':
-  command     => '/bin/sed -i "s/mysql_connect/mysqli_connect/" /var/www/html/index.php',
-  refreshonly => true,
-  notify      => Service['apache2'],
+  command => 'sed -i s/phpp/php/g /var/www/html/index.php',
+  path    => '/usr/local/bin/:/bin/',
+  notify  => Service['apache2'],
 }
 
-# Restart Apache after fixing the PHP error
 service { 'apache2':
-  ensure  => running,
-  require => Exec['fix_php_error'],
+  ensure => running,
 }
